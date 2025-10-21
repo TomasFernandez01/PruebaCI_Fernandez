67eebd01f78f22ef17540571782a1b45a0835f55
Tomas Fernandez <tomas.jobs.gr@gmail.com>
2025-10-20 23:52:40 -0300

    Arreglo 2do test para el pipeline

--- test_app.py
+++ test_app.py
@@ -11,6 +11,8 @@ def test_suma_es_correcta():
     assert resultado == 5  # caso base
 #   2- Forma logica
 def test_suma_mayor_que_operandos():
-    for a, b in [(0,1), (2,3), (10,5), (-1,4)]:
+    for a, b in [(0,1), (2,3), (10,5), (1,4)]:
         assert suma(a,b) >= a
         assert suma(a,b) >= b
+
+# par que dio error (-1,4)
